1592a2b1997383fee52bbdb8b4265bc19c35571c
Cbukuba <chrisbukuba058@gmail.com>
2021-09-11 12:19:17 +0200

    Exercise06

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,57 +6,42 @@
 /*   By: cbukuba <chrisbukuba058@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 23:58:17 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/09/09 12:19:35 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/09/11 12:12:44 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	write_chfr(int a, int b, int c, int d)
+/*void	ft_putchar(char c)
 {
-	char	tab[6];
+	write(1, &c, 1);
+}*/
 
-	tab[0] = a + '0';
-	tab[1] = b + '0';
-	tab[2] = ' ';
-	tab[3] = c + '0';
-	tab[4] = d + '0';
-	tab[5] = ',';
-	write(1, &tab, 6);
-}
-
-void	ft_print_comb2(void)
+void	ft_test(void)
 {
 	int	a;
 	int	b;
-	int	c;
-	int	d;
 
 	a = 0;
-	while (a <= 9)
+	while (a <= 99)
 	{
-		b = 0;
-		while (b <= 9)
+		b = 1;
+		while (b <= 99)
 		{
-			c = 0;
-			while (c <= 9)
-			{
-				d = 0;
-				while (d <= 9)
-				{
-					write_chfr(a, b, c, d);
-					d ++;
-				}
-				c ++;
-			}
+			ft_putchar(a % 10 + '0');
+			ft_putchar(a / 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			ft_putchar(',');
 			b ++;
 		}
-		a++;
+		a ++;
 	}
 }
 
-int	main(void)
+/*int	main(void)
 {
 	ft_print_comb2();
 	return (0);
-}
+}*/
